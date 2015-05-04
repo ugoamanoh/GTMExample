73e0018d462291a0ed25504756f53fd59039cbb5
ugoamanoh <ugo@dealdey.com>
2015-05-04 09:29:18 +0100

    Working Version: Downloading container initially

--- app/src/main/java/ex/gtm/com/Activity1.java
+++ app/src/main/java/ex/gtm/com/Activity1.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,19 +16,52 @@ import com.google.android.gms.tagmanager.Container;
 import com.google.android.gms.tagmanager.TagManager;
 import com.google.android.gms.tagmanager.ContainerHolder;
 
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
 
 public class Activity1 extends Activity {
 
     private static final long TIMEOUT_FOR_CONTAINER_OPEN_MILLISECONDS = 2000;
-    private static final String CONTAINER_ID = "GTM-5ZTMWG";
+    private static final String CONTAINER_ID = "GTM-TG8PJ9";
     private boolean containerLoaded = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        TagManager tagManager = TagManager.getInstance(this);
+
+        // Modify the log level of the logger to print out not only
+        // warning and error messages, but also verbose, debug, info messages.
+        tagManager.setVerboseLoggingEnabled(true);
+
+        PendingResult<ContainerHolder> pending =
+                tagManager.loadContainerPreferNonDefault(CONTAINER_ID,
+                        R.raw.defaultcontainer_binary);
+
+        // The onResult method will be called as soon as one of the following happens:
+        //     1. a saved container is loaded
+        //     2. if there is no saved container, a network container is loaded
+        //     3. the 2-second timeout occurs
+        pending.setResultCallback(new ResultCallback<ContainerHolder>() {
+            @Override
+            public void onResult(ContainerHolder containerHolder) {
+                ContainerHolderSingleton.setContainerHolder(containerHolder);
+                Container container = containerHolder.getContainer();
+                if (!containerHolder.getStatus().isSuccess()) {
+                    Log.e("GTMExample", "failure loading container");
+                    //displayErrorToUser(R.string.load_error);
+                    return;
+                }
+                ContainerHolderSingleton.setContainerHolder(containerHolder);
+                ContainerLoadedCallback.registerCallbacksForContainer(container);
+                containerHolder.setContainerAvailableListener(new ContainerLoadedCallback());
+                //startMainActivity();
+            }
+        }, TIMEOUT_FOR_CONTAINER_OPEN_MILLISECONDS, TimeUnit.MILLISECONDS);
 
 
         Button btn = (Button)(findViewById(R.id.button1));
@@ -46,6 +80,47 @@ public class Activity1 extends Activity {
     @Override
     protected void onStart() {
         super.onStart();
-        GtmUtils.pushOpenScreenEvent(this, "Activity1");
+        //GtmUtils.pushOpenScreenEvent(this, "Activity1");
+    }
+
+
+    private static class ContainerLoadedCallback implements ContainerHolder.ContainerAvailableListener {
+        @Override
+        public void onContainerAvailable(ContainerHolder containerHolder, String containerVersion) {
+            // We load each container when it becomes available.
+            Container container = containerHolder.getContainer();
+            registerCallbacksForContainer(container);
+        }
+
+        public static void registerCallbacksForContainer(Container container) {
+            // Register two custom function call macros to the container.
+            container.registerFunctionCallMacroCallback("increment", new CustomMacroCallback());
+            container.registerFunctionCallMacroCallback("mod", new CustomMacroCallback());
+            // Register a custom function call tag to the container.
+            container.registerFunctionCallTagCallback("custom_tag", new CustomTagCallback());
+        }
+    }
+
+    private static class CustomMacroCallback implements Container.FunctionCallMacroCallback {
+        private int numCalls;
+
+        @Override
+        public Object getValue(String name, Map<String, Object> parameters) {
+            if ("increment".equals(name)) {
+                return ++numCalls;
+            } else if ("mod".equals(name)) {
+                return (Long) parameters.get("key1") % Integer.valueOf((String) parameters.get("key2"));
+            } else {
+                throw new IllegalArgumentException("Custom macro name: " + name + " is not supported.");
+            }
+        }
+    }
+
+    private static class CustomTagCallback implements Container.FunctionCallTagCallback {
+        @Override
+        public void execute(String tagName, Map<String, Object> parameters) {
+            // The code for firing this custom tag.
+            Log.i("CuteAnimals", "Custom function call tag :" + tagName + " is fired.");
+        }
     }
 }
--- app/src/main/java/ex/gtm/com/GtmUtils.java
+++ app/src/main/java/ex/gtm/com/GtmUtils.java
@@ -15,6 +15,6 @@ public class GtmUtils {
 
     public static void pushOpenScreenEvent(Context context, String screenName) {
         DataLayer dataLayer = TagManager.getInstance(context).getDataLayer();
-        dataLayer.pushEvent("openScreen", DataLayer.mapOf("screenName", screenName));
+        dataLayer.pushEvent("event", DataLayer.mapOf("screenName", screenName));
     }
 }
